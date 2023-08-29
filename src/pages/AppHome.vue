d107aff1b405c84161ad70443214338e0b366060
giacomopaolini1 <giacomopaolini08@gmail.com>
2023-08-29 15:57:58 +0200

    feat: slider home

--- src/pages/AppHome.vue
+++ src/pages/AppHome.vue
@@ -1,13 +1,17 @@
 <template>
     <Jumbotron />
+    <SliderHome />
 </template>
 
 <script>
     import Jumbotron from '../components/layouts/Jumbotron.vue'
+    import SliderHome from '../components/ui/SliderHome.vue'
+
     export default {
         name: "AppHome",
         components: {
-            Jumbotron
+            Jumbotron,
+            SliderHome
         }
     }
 </script>
