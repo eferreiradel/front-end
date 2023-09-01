c9e1bea636234d9c0b8974245e56fa7afb0fc16b
giacomopaolini1 <giacomopaolini08@gmail.com>
2023-09-01 19:09:28 +0200

    fix ShowTeacher

--- src/pages/ShowTeacher.vue
+++ src/pages/ShowTeacher.vue
@@ -12,7 +12,7 @@
                     <h3 class="mt-3 p-4">Luogo del corso</h3>
                     <div class="d-flex flex-wrap gap-2">
                         <div v-if="teacherData.is_live_lesson" class="rounded-pill border px-3 py-2">
-                            <i class="fa-solid fa-location-pin"></i> In presenza
+                            <i class="fa-solid fa-location-pin"></i> In presenza {{ teacherData.city }}
                         </div>
                         <div v-if="teacherData.is_remote_lesson" class="rounded-pill border px-3 py-2">
                             <i class="fa-solid fa-video"></i> Via webcam
@@ -20,7 +20,7 @@
                     </div>
                 </div>
                 <div class="ps-4">
-                    <button class="my-btn-container-static btn my-5 py-2 my-rounded my-bgGreen">
+                    <button class="my-btn-container-static btn my-5 py-2 my-rounded my-bgGreen-btn">
                         CONTATTA
                     </button>
                 </div>
@@ -48,8 +48,8 @@
             
             <div class="order-1 order-md-1 order-lg-2 col-12 col-md-12 col-lg-5">
                 <div class="my-bgGreen my-rounded col-12 col-lg-8 offset-lg-4 mt-3 p-2">
-                    <div class="d-flex flex-column align-items-center text-white py-4 gap-3">
-                        <img :src="teacherData.profile_image" class="img-fluid rounded-circle mt-3 border border-3 border-white" alt="Nome Cognome">
+                    <div class="d-flex flex-column align-items-center text-light py-4 gap-3">
+                        <img :src="teacherData.profile_image" class="img-fluid rounded-circle mt-3 my-border-img" alt="Nome Cognome">
                         <h2>{{ teacherData.name }} {{ teacherData.surname }}</h2>
                         <p class="card-text m-0">
                             <span :class="teacherData.avg_vote >= 1 ? 'fa fa-star fs-5 checked' : 'fa fa-star fs-5'"></span>
@@ -59,19 +59,15 @@
                             <span :class="teacherData.avg_vote >= 5 ? 'fa fa-star fs-5 checked' : 'fa fa-star fs-5'"></span>
                         </p>
                         <p>({{ teacherData.total_votes }} voti)</p>
-                        <div class="d-flex gap-3 bg-light text-dark rounded-pill p-md-3 py-2 px-5 justify-content-around">
-                            <div class="d-flex flex-column align-items-center p-2">
-                                <h5>Città</h5>
-                                <span>{{ teacherData.city }}</span>
-                            </div>
-                            <div class="d-flex flex-column align-items-center p-2">
-                                <h5>Prezzo</h5>
-                                <span>{{ teacherData.price_for_hour }}€/ora</span>
+                        <div class="my-border  d-flex gap-3 bg-light text-dark rounded-pill p-md-3 py-2 px-5 justify-content-around">
+                            <div class="d-flex justify-content-center align-items-center p-2">
+                                <span>Tariffa oraria</span>
+                                <h5 class="ms-2 mb-0">{{ teacherData.price_for_hour }}€</h5>
                             </div>
                         </div>
                         <div class="d-flex text-dark flex-wrap justify-content-center align-items-center gap-2 p-md-3">
                             <template v-for="subject in teacherData.subjects">
-                                <div class="subject bg-light py-2 px-3 rounded-pill border">{{subject.name}}</div>
+                                <div class="my-border subject bg-light py-2 px-3 rounded-pill">{{subject.name}}</div>
                             </template>
                         </div>
                     </div>
@@ -79,7 +75,7 @@
             </div>
         </div>
         
-        <div class="d-flex flex-column gap-5 py-5 col-12">
+        <div class="d-flex flex-column gap-5 mb-5 py-5 col-12">
             <div class="d-flex flex-column gap-2">
                 <h3 class="ps-5">Recensioni</h3>
                 <div class="p-4 ps-5">
@@ -127,7 +123,7 @@
         <div class="container">
             <div class="row">
                 <div class="mx-auto d-flex justify-content-center align-items-center">
-                    <button class="w-50 my-btn-container btn my-3 fs-5 py-3 position-fixed bottom-0 my-rounded my-bgGreen w-100 text-center">
+                    <button class="w-50 my-btn-container btn my-3 fs-5 py-3 position-fixed bottom-0 my-rounded my-bgGreen-btn w-100 text-center">
                         CONTATTA
                     </button>
                 </div>
@@ -140,24 +136,35 @@
 </template>
 
 <script>
-    import {store} from '../store'
+    import { store } from '../store';
 
     export default {
         name: "ShowTeacher",
         data() {
-            return {
-                store,
-                teacherData: null
+        return {
+            store,
+            teacherData: null
+        }
+        },
+        methods: { 
+        handleScroll() {
+            const button = document.querySelector('.my-btn-container');
+            if (window.scrollY > 600) {
+            button.style.display = 'block';
+            } else {
+            button.style.display = 'none';
             }
+        }
         },
-        methods:{ 
-
+        beforeDestroy() {
+        window.removeEventListener('scroll', this.handleScroll);
         },
-        mounted(){
-            store.getUserById(this.$route.params.userId).then(userData => {
-                this.teacherData = userData;
-                console.log(this.teacherData);
-            });
+        mounted() {
+        store.getUserById(this.$route.params.userId).then(userData => {
+            this.teacherData = userData;
+            console.log(this.teacherData);
+        });
+            window.addEventListener('scroll', this.handleScroll);
         }
     }
 </script>
@@ -165,8 +172,14 @@
 <style scoped style lang="scss">
     @import "../style.scss";
 
-    .my-bgGreen{
+    .my-bgGreen {
+        background-color: $color__primary;
+        border: 1px solid $color__dark;
+    }
+
+    .my-bgGreen-btn {
         background-color: $color__secondary;
+        border: 1px solid $color__dark;
     }
 
     img{
@@ -181,6 +194,8 @@
         color: $color__dark;
         max-width: 300px;
         border: 1px solid $color__dark;
+        display: none;
+        transition: opacity 0.4s ease-out, transform 0.4s ease-out;
     }
     .my-btn-container:hover {
         background-color: $color__light !important;
@@ -198,6 +213,19 @@
         background-color: $color__light !important;
         color: #000;
         border: 1px solid #000;
+    }
+
+    .my-border {
+        border: 1px solid $color__dark !important;
+        border-radius: 15px;
+    }
+
+    .my-border-img {
+        border: 3px solid $color__light !important;
+    }
+
+    h2 {
+        font-weight: 700;
     }
 
 </style>
